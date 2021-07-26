e2d6cbb6ce30e0bf92488228a1e9faf90971a3f0
callingpizza <68723482+callingpizza@users.noreply.github.com>
2021-07-26 09:29:03 +0530

    Updated file structure

--- src/face_detector.py
+++ src/face_detector.py
@@ -2,13 +2,14 @@ import cv2 as cv
 import numpy as np
 from utils import turn_gray
 
+FACE_CASCADE = cv.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+
 def detect_faces_in_image(image_path: str) -> None:
     image_color = cv.imread(image_path)
     image_gray = turn_gray(image_color)
-    face_cascade = cv.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 
     # Returns rectangular coordinates of the detected face based on scaleFactor and minNeighbors
-    faces_rect = face_cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=3)
+    faces_rect = FACE_CASCADE.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=3)
 
     # For each coordinate in the faces_rect, draw a rectangle
     for (x, y, w, h) in faces_rect:
