3cbb64a717e3edbd7b6173e36c5e3a4cfb8b99eb
callingpizza <68723482+callingpizza@users.noreply.github.com>
2021-07-28 13:33:54 +0530

    detector class comments

--- Recognition/Detector.hpp
+++ Recognition/Detector.hpp
@@ -15,7 +15,7 @@
 namespace recognition {
 
   /// <summary>
-  /// 
+  /// Face detection class.
   /// </summary>
   class detector {
     std::string m_ImagePath;
@@ -24,13 +24,23 @@ namespace recognition {
     detector();
     
     /// <summary>
-    /// 
+    /// Sets path values.
     /// </summary>
-    /// <param name="image_path"></param>
-    /// <param name="video_path"></param>
+    /// <param name="image_path">Detector's image path.</param>
+    /// <param name="video_path">Detector's video path.</param>
     detector(const std::string& image_path, const std::string& video_path);
 
+    /// <summary>
+    /// Detects faces in an image. (use only if m_ImagePath is not set)
+    /// </summary>
+    /// <param name="image_path">The path to the image.</param>
+    /// <param name="window_name">Name of the window.</param>
     void detect_faces_in_image(const std::string& image_path, const std::string& window_name);
+
+    /// <summary>
+    /// Detects faces in an image.
+    /// </summary>
+    /// <param name="window_name">Name of the window.</param>
     void detect_faces_in_image(const std::string& window_name);
   };
 
