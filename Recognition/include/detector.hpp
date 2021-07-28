ed735efcd4f9265d288803b7c42e81782f70eb69
callingpizza <68723482+callingpizza@users.noreply.github.com>
2021-07-28 20:47:15 +0530

    Indentation fix

--- Recognition/include/detector.hpp
+++ Recognition/include/detector.hpp
@@ -4,47 +4,38 @@
 #include <vector>
 
 #ifdef __cplusplus
-  #include <opencv2/opencv.hpp>
-  #include <cstdio>
+    #include <opencv2/opencv.hpp>
+    #include <cstdio>
 
-  using namespace cv;
+    using namespace cv;
 #else
-  #error C++ only
+    #error C++ only
 #endif
 
-namespace recognition {
-
-  /// <summary>
-  /// Face detection class.
-  /// </summary>
-  class Detector {
-    std::string m_ImagePath;
-    std::string m_VideoPath;
-
-    const std::string m_FaceCascadePath = "cascades/haarcascade_frontalface_default.xml";
-  public:
-    Detector();
-    
+namespace recognition 
+{
     /// <summary>
-    /// Sets path values.
+    /// Face detection class.
     /// </summary>
-    /// <param name="image_path">Detector's image path.</param>
-    /// <param name="video_path">Detector's video path.</param>
-    Detector(const std::string& image_path, const std::string& video_path);
+    class Detector 
+    {
+        const std::string m_faceCascadePath = "cascades/haarcascade_frontalface_default.xml";
+    public:
+        Detector();
 
-    /// <summary>
-    /// Detects faces in an image. (use only if m_ImagePath is not set)
-    /// </summary>
-    /// <param name="image_path">The path to the image.</param>
-    /// <param name="window_name">Name of the window.</param>
-    void detectFacesInImage(const std::string& image_path, const std::string& window_name);
+        /// <summary>
+        /// Detects faces in an image. (use only if m_ImagePath is not set)
+        /// </summary>
+        /// <param name="imagePath">The path to the image.</param>
+        /// <param name="windowName">Name of the window.</param>
+        void detectFacesInImage(const std::string& imagePath, const std::string& windowName);
 
-    /// <summary>
-    /// Detects faces in an image.
-    /// </summary>
-    /// <param name="window_name">Name of the window.</param>
-    void detectFacesInImage(const std::string& window_name);
-  };
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="videoPath"></param>
+        /// <param name="windowName"></param>
+        void detectFacesInVideo(const std::string& videoPath, const std::string& windowName);
+    };
 }
 
--- Recognition/main.cc
+++ Recognition/main.cc
@@ -1,6 +1,6 @@
 #include "detector.hpp"
 
 int main() {
-  recognition::Detector detector;
-  detector.detectFacesInImage("res/rdj.jpg", "ee");
+    recognition::Detector detector;
+    detector.detectFacesInImage("res/rdj.jpg", "ee");
 }
